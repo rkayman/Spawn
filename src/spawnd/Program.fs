f40a04b1fa1c6b060800d2d4cc2070c1896dbfe1
Rob Kayman <rkayman@gmail.com>
2019-09-09 10:12:09 -0700

    Implement new actor (mailbox processor) approach to alarms
    Incorporate structure changes to use Spawn.IO namespace and leverage actor-async approach

--- src/spawnd/Program.fs
+++ src/spawnd/Program.fs
@@ -1,5 +1,8 @@
 namespace Spawn
 
+open Scheduler
+open Spawn.IO.Messages
+open Spawn.IO.Configuration
 open System
 open System.IO
 open System.Threading
@@ -7,27 +10,14 @@ open System.Threading
 module Program =
 
     module CommandLine =
-
-        type ListOptions = | ByDomain | ByName
-        type RemoveOptions =
-            | Domain of string
-            | Name of string
-            | Id of int
-
-        type CommandLineCommand =
-            | Import of FileInfo
-            | List of ListOptions
-            | Remove of RemoveOptions
-            | Dump
-            | Help
-
+        
         let private badCommand extraMessage = eprintfn "Bad command %s" extraMessage; Help
 
-        let private toLower (s: string) = s.ToLowerInvariant()
+        let inline private toUpper (s: string) = s.ToUpper()
+        
+        let inline private equals (x: string) (y: string) = x.Equals(y, StringComparison.OrdinalIgnoreCase)
 
         let inline private stripQuotes (s: string) = s.Replace("\"", String.Empty, StringComparison.InvariantCulture)
-
-        let private parseDump _ = Dump
 
         let private parseHelp _ = Help
 
@@ -38,25 +28,26 @@ module Program =
 
         let private parseList args =
             match args with
-            | [] -> List ByDomain
-            | x :: [] when toLower x = "bydomain" -> List ByDomain
-            | x :: [] when toLower x = "byname" -> List ByName
+            | [] -> List (ByDomain None)
+            | x :: [] when x |> equals "ByDomain" -> ByDomain None |> List
+            | x :: [] when x |> equals "ByName"   -> ByName None   |> List
+            | x :: y :: [] when x |> equals "ByDomain" -> Some y |> ByDomain |> List
+            | x :: y :: [] when x |> equals "ByName"   -> Some y |> ByName   |> List
             | _ -> badCommand ""
 
         let private parseRemove args =
             match args with
             | [] -> badCommand ""
-            | x :: y :: [] when toLower x = "domain" -> Remove(Domain(stripQuotes y))
-            | x :: y :: [] when toLower x = "name" -> Remove(Name(stripQuotes y))
-            | x :: y :: [] when toLower x = "id" -> Remove(Id(int y))
+            | x :: y :: [] when x |> equals "ByDomain" -> stripQuotes y |> Some |> ByDomain |> Remove
+            | x :: y :: [] when x |> equals "ByName"   -> stripQuotes y |> Some |> ByName   |> Remove
             | _ -> badCommand ""
 
         let private commandMap =
-            [ ("import", parseImport); ("list", parseList); ("remove", parseRemove); ("dump", parseDump);
-              ("help", parseHelp); ("/h", parseHelp); ("-h", parseHelp); ("-?", parseHelp); ("/?", parseHelp) ]
+            [ ("IMPORT", parseImport); ("LIST", parseList); ("REMOVE", parseRemove);
+              ("HELP", parseHelp); ("/h", parseHelp); ("-h", parseHelp); ("-?", parseHelp); ("/?", parseHelp) ]
             |> Map.ofSeq
 
-        let private isCommand str = str |> toLower |> commandMap.ContainsKey
+        let private isCommand str = str |> toUpper |> commandMap.ContainsKey
 
         let parseCommandLine args =
             match args with
@@ -64,13 +55,11 @@ module Program =
             | x :: xs when isCommand x ->
                 let invApply x f = f x
                 let parser = invApply xs
-                commandMap |> Map.find x |> parser
+                commandMap |> Map.find (toUpper x) |> parser
             | _ -> badCommand ""
 
     [<EntryPoint>]
     let main _ =
-
-        use cts = new CancellationTokenSource()
         
         let makeArg (cs: char list) (isQuoted: bool) (xs: string list) =
             if isQuoted then failwith "Illegal input. Missing closing quote."
@@ -93,23 +82,64 @@ module Program =
                 
             str.ToCharArray() |> Seq.fold folder ([], false, [])
                               |> fun (x, y, z) -> makeArg x y z
-                              |> fun (x, y, z) -> z
+                              |> fun (_, _, z) -> z
 
-        let console() = async {
-            let ws = [| ' '; '\t'; '\n'; '\r' |]
-            let cancel _ = printfn "Quitting console..."
+        use cts = new CancellationTokenSource()
+
+        let console (token: CancellationTokenSource) = async {
+            use actor = new AgendaActor(token.Token)
+            
+            let cancel _ =
+                printfn "Quitting console... IsCancellationRequested? %b" token.IsCancellationRequested
+                actor.Dispose()
+                Async.Sleep(2000) |> Async.RunSynchronously
             do AppDomain.CurrentDomain.ProcessExit.Add cancel
+            
+            let limit len (str: string) =
+                match str with
+                | s when s.Length <= len -> s
+                | s -> s.Substring(0, len - 1) |> sprintf "%s…"
 
-            while not cts.IsCancellationRequested do
-                printf "Command > "
+            while not token.IsCancellationRequested do
+                printf "Command :> "
                 let input = Console.ReadLine()
                 let args = makeArgs input
-                printfn "%A" args
                 let cmd = CommandLine.parseCommandLine args
-                printfn "%A" cmd
+                
+                match cmd with
+                | Import fi ->
+                    try
+                        let! json = File.ReadAllTextAsync(fi.FullName, Text.Encoding.UTF8) |> Async.AwaitTask
+                        let agenda = readAgenda json |> function Ok x -> x | Error e -> raise (new FormatException(e.ToString()))
+                        let! response = actor.Process(agenda)
+                        printfn "Processed %i alarms" response
+                    with
+                    | e -> eprintfn "[ERROR] %A" e; return ()
+                    
+                | List opt ->
+                    let sortDomain (xs: seq<Guid * AlarmKey>) = xs |> Seq.sortBy (fun (x,y) -> y.domain) 
+                    let sortName (xs: seq<Guid * AlarmKey>) = xs |> Seq.sortBy (fun (x,y) -> y) 
+                    let! response =
+                        match opt with
+                        | ByDomain filter -> actor.ListDomain(filter)
+                        | ByName filter   -> actor.ListName(filter)
+                    response |> Seq.map (fun (id, ak) -> (id.ToString("N").Substring(19),
+                                                          { domain = limit 12 ak.domain; name = limit 30 ak.name }))
+                             |> Seq.iter (fun (id, ak) -> printfn "%-12s | %-12s | %-30s" id ak.domain ak.name)
+                    response |> Seq.length |> printfn "%d items"
+                    
+                | Remove opt ->
+                    let! response =
+                        match opt with
+                        | ByDomain filter -> actor.RemoveDomain(filter)
+                        | ByName filter   -> actor.RemoveName(filter)
+                    printfn "Removed %i alarms" response
+                    do! Async.Sleep(300)
+                    
+                | Help -> ()
         }
 
-        console () |> Async.Start
+        console cts |> Async.Start
 
         waitForShutdown()
         0
--- src/spawnd/Scheduler.fs
+++ src/spawnd/Scheduler.fs
@@ -1,158 +1,181 @@
 namespace Spawn
 
-open NodaTime
-open System.IO
-open System.Text
+open System.Collections.Generic
+open System.Collections.Concurrent
 open System.Threading
 open System
-open Spawn.Clock.Alarm
 
- (*
- 
- What does the scheduler do?
- 
- Use Cases:
- 0. Daemon starts (** entry point **)
- 1. Scheduler starts
- 2. Scheduler (aka ScheduleManager) receives a configuration request (e.g. add, update, remove)
- 3. Scheduler receives a schedule request (e.g. start, stop)
- 4. Scheduler receives alarm from schedule
- 5. System Event interrupts Daemon/Scheduler (e.g. OS startup / shutdown; process failure)
- 
- Rules:
- 0.  StartDaemon
-     - initialize app => start top-level dependencies: { in-memory bus; scheduler (aka ScheduleManager) }
-        - scheduler initialization will subscribe to in-memory bus topic(s)
-            * https://blog.mavnn.co.uk/an-in-memory-message-bus-in-100-lines-or-less/
-     - let env = Map of environment settings: configFileName and envFolderLocations
-     - raise DaemonStarted with let guid = new Guid; { id = guid; causation = Guid.Empty;
-                                                       correlation = guid; body = env }
-     - log DaemonStarted
- 
- 1.  OnDaemonStarted
-     - find configFileName in envFolderLocations
-     - read and parse config
-     - raise SchedulerStarted with let guid = new Guid; { id = guid; causation = id'; correlation = correlation';
-                                                          body = alarmConfigList }
-     - log SchedulerStarted
-
- 2.  OnSchedulerStarted
-     - scan for, log/report, and remove duplicate configurations
-     - for each config in configList
-         - raise ScheduleAlarmRequested with let guid = new Guid; { id = guid; causation = id';
-                                                                    correlation = correlation'; body = alarmConfig }
-         - log ScheduleAlarmRequested
-     
- 3.  OnScheduleAlarmRequested
-     - schedule (categorize/classify) alarm
-     - if schedule (category) exists then add alarm to schedule.alarms collection using IgnoreAlarm
-     - else create new schedule and corresponding alarms collection then add alarm using IgnoreAlarm
-     - raise AlarmScheduled with let guid = new Guid; { id = guid; causation = id'; correlation = correlation';
-                                                        body = alarmConfig }
-     - log AlarmScheduled
-
- 4.  OnAlarmScheduled
-     - if autoStart = true then
-        - change alarm state in schedule.alarms using TriggerAlarm
-        - start alarm
-        - raise StartAlarmRequested with let guid = new Guid; { id = guid; causation = id'; correlation = correlation';
-                                                                body = ??? }
-        - log StartAlarmRequested
-
- 5.  OnStartAlarmRequested
-     - change alarm state in schedule.alarms using TriggerAlarm
-     - start alarm
-     - raise AlarmStarted with let guid = new Guid; { id = guid; causation = ???; correlation = alarm.id }
-     - log AlarmStarted
-          
- 6.  OnAlarmStarted
-     - tbd
-  
- 7.  OnStopAlarmRequested
-     - change alarm state in schedule.alarms using IgnoreAlarm
-     - stop alarm
-     - raise AlarmStopped with let guid = new Guid; { id = guid; causation = ???; correlation = alarm.id }
-     - log AlarmStopped
-        
- 8.  OnAlarmStopped
-     - tbd
-     
- 9.  OnScheduleTriggered
-     - for each alarm in schedule.alarms
-        - call TriggerAlarm (send payload to alarm.uri)
-        - raise AlarmTriggered with let guid = new Guid; { id = guid; causation = schedule.id; correlation = ??? }
-      
- 
- *)
-    
 module Scheduler =
-    open Messaging
-    
-    let private cts = new CancellationTokenSource()
-    let private timeout = 10000
+    open Spawn.IO.Configuration
 
-    // TODO: split into many agents -- one to control workflow, and one agent for each step in workflow
-//    let rec private loop (agent: SchedulerMessage Agent) =
-//        async {
-//            try
-//                let! msg = agent.TryReceive(timeout)
-//                match msg with
-//                | None -> return! loop agent
-//                | Some msg' ->
-//                    match msg' with
-//                    | StartScheduler locations ->
-//                        let guid = Guid.NewGuid()
-//                        let evt = { id = guid; causation = guid; correlation = guid;
-//                                    timestamp = Instant.FromDateTimeOffset(DateTimeOffset.Now);
-//                                    name = "SchedulerStarted"; body = locations }
-//                        agent.Post(SchedulerStarted evt)
-//                        return! loop agent
-//
-//                    | SchedulerStarted evt ->
-//                        let path = evt.body |> Seq.tryFind selectFirstFileFound
-//                        match path with
-//                        | None ->
-//                            let fmtToBuf sb uri = Printf.bprintf sb "\n%A" uri; sb
-//                            let xs = evt.body |> Seq.fold fmtToBuf (StringBuilder())
-//                            let errmsg = sprintf "Unable to find 'spwncfg.json in: %A" xs
-//                            let guid = Guid.NewGuid()
-//                            let evt' = { id = guid; causation = evt.causation; correlation = evt.correlation;
-//                                         timestamp = Instant.FromDateTimeOffset(DateTimeOffset.Now);
-//                                         name = "SchedulerFaulted"; body = FileNotFoundException(errmsg) :> exn }
-//                            agent.Post(SchedulerFaulted evt')
-//                            return! loop agent
-//                        
-//                        | Some p ->
-//                            // TODO: Read and parse json configuration file
-//                            printfn "Received SchedulerStarted message"
-//                            return! loop agent
-//                                
-//                    | SchedulerConfigured evt -> return! loop agent
-//                    | ScheduleAlarm cfg -> return! loop agent
-//                    | AlarmScheduled evt -> return! loop agent
-//                    | StartAlarm ids -> return! loop agent
-//                    | AlarmStarted evt -> return! loop agent
-//                    | StopAlarm ids -> return! loop agent
-//                    | AlarmStopped evt -> return! loop agent
-//                    | TriggerAlarm ids -> return! loop agent
-//                    | AlarmTriggered evt -> return! loop agent
-//                    | SchedulerFaulted evt -> return! loop agent
-//                    
-//            finally
-//                // TODO: convert to informational logging
-//                printfn "Scheduler agent exited."
-//        }
-//        
-//    let private agent = Agent<_>.Start(loop, cts.Token)
-//    
-//    type Scheduler() = class end with
-//        static member StartScheduler(locations) = agent.Post()
-//        static member ScheduleAlarm(config) = agent.Post()
-//        static member StartAlarm(alarmIds) = agent.Post()
-//        static member StopAlarm(alarmIds) = agent.Post()
-//            
-//        interface IDisposable with
-//            member x.Dispose() =
-//                cts.Cancel()
-//                (agent :> IDisposable).Dispose()
+    type Agent<'T> = MailboxProcessor<'T>
+    
+    type internal AutoCancelActor<'T>(body: (Agent<'T> -> Async<unit>), ?token: CancellationToken) =
+        let cts = token |> Option.map (fun t -> CancellationTokenSource.CreateLinkedTokenSource(t))
+                        |> Option.defaultValue (new CancellationTokenSource())
+        let mutable disposed = false
         
+        let actor = Agent<'T>.Start(body, cts.Token)
+        
+        member internal this.Agent with get() = actor
+        
+        member internal this.Token with get() = cts.Token
+        
+        abstract member Dispose : unit -> unit
+        default this.Dispose() = (this :> IDisposable).Dispose()
+
+        interface IDisposable with
+            member this.Dispose() =
+                if not disposed then
+                    disposed <- true
+                    cts.Cancel()
+                    (actor :> IDisposable).Dispose()
+                    cts.Dispose()
+
+    let private contains (value: string option) (source: string) =
+        value |> Option.map (fun x -> source.Contains(x, StringComparison.OrdinalIgnoreCase))
+              |> Option.defaultValue true
+    
+    type internal CommandOption = Domain of string option | Name of string option
+    
+    let private (|ByDomain|ByName|) x =
+        match x with
+        | Domain domain -> ByDomain domain
+        | Name name     -> ByName name
+        
+    type private AlarmCommand =
+        | Schedule of Alarm
+        | Info of CommandOption * AsyncReplyChannel<(Guid * AlarmKey) option>
+            
+    let private alarmStorage (inbox: Agent<_>) =
+        let alarmId = Guid.NewGuid()
+        
+        let mutable cnt = 0
+        let rec loop id alarm = async {
+            
+            try
+                let! msg = inbox.Receive()
+                match msg with
+                | Schedule ax ->
+                    return! loop id (Some ax)
+                
+                | Info (option, channel) ->
+                    match alarm with
+                    | None -> channel.Reply(None)
+                    | Some ax ->
+                        match option with
+                        | ByDomain value -> ax.domain |> contains value
+                        | ByName value -> ax.name |> contains value
+                        |> function
+                            | true -> channel.Reply(Some (alarmId, { domain = ax.domain; name = ax.name }))
+                            | false -> channel.Reply(None)
+                    return! loop id alarm
+                    
+            finally
+                if cnt < 1 then
+                    alarmId.ToString("N").Substring(19) |> printfn "Completed Alarm agent: %s"
+                    cnt <- cnt + 1
+        }
+        loop alarmId None
+            
+    type private AlarmActor(?token: CancellationToken) =
+        inherit AutoCancelActor<AlarmCommand>(alarmStorage, ?token = token)
+        
+        with
+            member this.Schedule(alarm) = this.Agent.Post(Schedule alarm)
+            
+            member this.InfoFilteredBy(filter) =
+                let buildMessage = fun ch -> Info (filter, ch)
+                this.Agent.PostAndAsyncReply(buildMessage)
+        
+    type internal AgendaCommand =
+        | Process of Agenda * AsyncReplyChannel<int>
+        | Remove of CommandOption * AsyncReplyChannel<int>
+        | List of CommandOption * AsyncReplyChannel<seq<Guid * AlarmKey>>
+    
+    type private AlarmValue = AlarmActor
+    type private AlarmDB = ConcurrentDictionary<AlarmKey, AlarmValue>
+    type private AlarmKVP = KeyValuePair<AlarmKey, AlarmValue>
+    
+    let private alarmAccess (token: CancellationToken) (inbox: Agent<_>) =
+        let alarms = AlarmDB(4, 1200)
+        
+        let removeAlarms (filter: AlarmKVP -> bool) (xs: AlarmDB) =
+            let remove cnt (value: IDisposable) = value.Dispose(); cnt + 1
+            let numRemoved =
+                xs |> Seq.filter filter
+                   |> Seq.map (fun t -> xs.TryRemove(t.Key))
+                   |> Seq.filter fst
+                   |> Seq.fold (fun s (_,t) -> remove s t) 0
+            numRemoved, xs
+
+        let folder (s: AlarmDB * int) (t: Alarm) =
+            let key = { domain = t.domain; name = t.name }
+            let dict, cnt = s
+            let makeValue _ = new AlarmActor(token)
+            let actor = dict.GetOrAdd(key, makeValue)
+            actor.Schedule(t)
+            dict, cnt + 1
+        
+        let mutable cnt = 0
+        let rec loop xs = async {
+            
+            try
+                let! msg = inbox.Receive()
+                match msg with
+                | Process (agenda, channel) ->
+                    let xs', cnt = agenda.alarms |> Seq.fold folder (xs, 0)
+                    channel.Reply(cnt)
+                    return! loop xs'
+                
+                | Remove (ByDomain value, channel) ->
+                    let numRemoved, xs' =
+                        xs |> removeAlarms (fun x -> x.Key.domain |> contains value)
+                    channel.Reply(numRemoved)
+                    return! loop xs'
+                
+                | Remove (ByName value, channel) ->
+                    let numRemoved, xs' =
+                        xs |> removeAlarms (fun x -> x.Key.name |> contains value)
+                    channel.Reply(numRemoved)
+                    return! loop xs'
+
+                | List (option, channel) ->
+                    let! results = xs |> Seq.map (fun t -> t.Value.InfoFilteredBy(option))
+                                      |> Async.Parallel
+                    results |> Seq.filter (Option.isSome) |> Seq.map (Option.get) |> channel.Reply
+                    return! loop xs
+                
+            finally
+                if cnt < 1 then
+                    printfn "Completed Agenda agent"
+                    xs |> Seq.iter (fun t -> t.Value.Dispose())
+                    cnt <- cnt + 1
+                
+        }
+        loop alarms
+        
+    type internal AgendaActor(token) =
+        inherit AutoCancelActor<AgendaCommand>(alarmAccess token, token)
+        
+        with
+            member this.Process(agenda) =
+                let buildMessage = fun ch -> Process (agenda, ch)
+                this.Agent.PostAndAsyncReply(buildMessage)
+        
+            member this.RemoveDomain(filter) =
+                let buildMessage = fun ch -> Remove (Domain filter, ch)
+                this.Agent.PostAndAsyncReply(buildMessage)
+
+            member this.RemoveName(filter) =
+                let buildMessage = fun ch -> Remove (Name filter, ch)
+                this.Agent.PostAndAsyncReply(buildMessage)
+
+            member this.ListDomain(filter) =
+                let buildMessage = fun ch -> List (Domain filter, ch)
+                this.Agent.PostAndAsyncReply(buildMessage)
+                
+            member this.ListName(filter) =
+                let buildMessage = fun ch -> List (Name filter, ch)
+                this.Agent.PostAndAsyncReply(buildMessage)
+    
