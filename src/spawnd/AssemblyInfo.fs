38e34cc6f9f9f2b69727a82d3af938523d28d964
Rob Kayman <rkayman@gmail.com>
2019-09-15 15:06:59 -1000

    Use project properties to add assembly versioning

--- src/spawnd/AssemblyInfo.fs
+++ src/spawnd/AssemblyInfo.fs
@@ -9,7 +9,7 @@ open System.Runtime.CompilerServices
 [<assembly: AssemblyFileVersion("1.0.0.0")>]
 [<assembly: AssemblyDescription("Spawn alarm clock support for alarm schedules")>]
 [<assembly: AssemblyCopyright("Copyright 2018 Robert Kayman. All rights reserved.")>]
-[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyVersion("1.0.0.*")>]
 
 [<assembly: ComVisible(false)>]
 [<assembly: CLSCompliant(false)>]
